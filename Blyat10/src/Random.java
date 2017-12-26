fe220a2b8ec98e25610194d0355f9a0585d5a6a4
DerOtt <4050793+DerOtt@users.noreply.github.com>
2017-12-26 23:01:16 +0100

    cat walked over the keyboard

--- Blyat10/src/Random.java
+++ Blyat10/src/Random.java
@@ -2,8 +2,30 @@
 public class Random {
 
 	public static void main(String[] args) {
-		//eh is this working mate?
-		//hopefully this change will be shown as mine
+efaaaaaaaaaaa
+ads
+a
+sf
+ag
+e
+ga
+d
+a
+fa
+fa
+f
+ef
+
+g
+a
+
+de
+afd
+ea
+f
+af
+
+
 	}
 
 }
